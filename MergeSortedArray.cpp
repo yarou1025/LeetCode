d1c8965ce79f4acf6fe7f41390314efbafceb4b9
yarou1025 <50448374+yarou1025@users.noreply.github.com>
2022-05-03 10:24:11 +0800

    Update MergeSortedArray.cpp

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -12,4 +12,20 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
             nums1[indexMerge--] = nums2[index2--];
         }
     }
+    // int n1 = 0, n2 = 0;
+        // while(n1 < m and n2 < n){
+        //     if(nums1[n1] < nums2[n2]){
+        //         tmp.push_back(nums1[n1++]);                
+        //     }
+        //     else{
+        //         tmp.push_back(nums2[n2++]);
+        //     }
+        // }
+        // for(; n1 < m; n1++){
+        //     tmp.push_back(nums1[n1]);
+        // }
+        // for(; n2 < n; n2++){
+        //     tmp.push_back(nums2[n2]);
+        // }
+        // nums1.assign(tmp.begin(), tmp.end());
 }
