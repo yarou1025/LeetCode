ba55c418db35ee5a3dcb478e7e93b2f80772e722
yarou1025 <50448374+yarou1025@users.noreply.github.com>
2022-05-09 19:30:18 +0800

    Create 524.LongestWordinDictionarythroughDeleting.cpp

---
+++ 524.LongestWordinDictionarythroughDeleting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& dictionary) {
+        string longest = "";
+        for(int i = 0; i < dictionary.size(); i++){
+            if(dictionary[i].length() >= longest.length()){
+                if(check_contains(s, dictionary[i])){
+                    if(dictionary[i].length() > longest.length())
+                        longest = dictionary[i];
+                    else if(longest > dictionary[i]) longest = dictionary[i];
+                }
+            }
+            
+        }
+        return longest;
+    }
+    bool check_contains(string &ans, string &can){
+        int i = 0;
+        int j = 0;
+        while(i < ans.length() and j < can.length()){
+           if(ans[i++] == can[j]){
+               j++;
+           } 
+        }
+        if(j != can.length()) return false;
+        else return true;
+    }
+};
