a7322933a3bf10e86d2c3e7624e9c0704bd79eea
yarou1025 <50448374+yarou1025@users.noreply.github.com>
2022-05-17 00:50:34 +0800

    Update 0075.SortColors.cpp

--- 0075.SortColors.cpp
+++ 0075.SortColors.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> colors(300, 0);
+        vector<int> colors(3, 0);
         for(int i = 0; i < nums.size(); i++){
             colors[nums[i]] += 1;
         }
         int k = 0;
-        for( int i = 0; i < 300; i++){
+        for( int i = 0; i < 3; i++){
             for(int j = 0; j < colors[i]; j++){
                 nums[k++] = i;
             }
@@ -14,5 +14,6 @@ public:
         
     }
 };
+
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Sort Colors.
 Memory Usage: 8.5 MB, less than 25.30% of C++ online submissions for Sort Colors.
